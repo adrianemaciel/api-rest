1740e0174b09930929dc5b5bdf0efd7482179c6f
adrianemaciel <adrianemaciel.developer@gmail.com>
2024-01-29 17:22:35 -0300

    get route creation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-// importanto expresse atrevés do require
-const express = require("express");
+import express from "express";
 const app = express();
 
-// definição de uma porta
-const port = 3000;
+// mock
+const selecoes = [
+  { id: 1, selecao: "Brasil", grupo: "G" },
+  { id: 2, selecao: "Suíça", grupo: "G" },
+  { id: 3, selecao: "Sérvia", grupo: "G" },
+  { id: 4, selecao: "Camarões", grupo: "G" },
+];
 
 // rota padrão ou raiz
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Curso de Node Js");
 });
 
-// escutar a porta 3000
-app.listen(port, () => {
-  console.log(`Servidor rodando na endereço http://localhost:${port}`);
+app.get("/selecoes", (req, res) => {
+  res.status(200).send(selecoes);
 });
+
+export default app;
