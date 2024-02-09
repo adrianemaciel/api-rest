f0b5a29ad759a695de84b9f847d00cd268bcf3b4
adrianemaciel <adrianemaciel.developer@gmail.com>
2024-02-09 15:23:23 -0300

    refactor repository

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,22 @@ const conexao = mysql.createConnection({
 
 conexao.connect();
 
+/**
+ * executa um código sql com ou sem valores
+ * @param {string} sql instrução sql a ser executada
+ * @param {string=id | [selecao, id]} valores a serem passadas para o sql
+ * @param {string} mensagemReject mensagem a ser exibida
+ * @returns objeto da Promisse
+ */
+
+export const consulta = (sql, valores = "", mensagemReject) => {
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, valores, (erro, resultado) => {
+      if (erro) return reject(mensagemReject);
+      const row = JSON.parse(JSON.stringify(resultado));
+      return resolve(row);
+    });
+  });
+};
+
 export default conexao;
